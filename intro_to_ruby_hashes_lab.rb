59bceee0486472048ecf635129726cdfd21f5981
Andre Skeene <halodre24@gmail.com>
2020-02-21 02:08:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,8 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads: {:pieces => 4},{:names => hash}} 
-
+	monopoly = {railroads: {:pieces => 4} 
+  monopoly[:railroads] = name: {}
 end
 
 def monopoly_with_third_tier
